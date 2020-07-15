e1b69e99824610e2c0d4006b71883056ce96617b
maria.cano <maria.cano@kaleidos.net>
2020-07-15 12:14:00 +0200

    nav from categoryList to categoryDetail

--- lib/category_list.dart
+++ lib/category_list.dart
@@ -1,35 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'theme.dart';
+
+import 'category_detail.dart';
 
 class CategoryList extends StatelessWidget {
-  final categoryCard = Card(
+  Card _categoryCard(BuildContext context) {
+    return Card(
+      child: new InkWell(
+          onTap: () {
+            print('tapped');
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => CategoryDetail()));
+          },
+          child: Container(
+              padding: const EdgeInsets.all(25),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.only(right: 20),
+                      child: Icon(Icons.add_circle,
+                          size: 40, color: Colors.black)),
+                  Expanded(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                        Text(
+                          'Categoría',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        Text('6 recursos')
+                      ]))
+                ],
+              ))),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       elevation: 10,
       shadowColor: Colors.grey[100],
-      child: Container(
-          padding: const EdgeInsets.all(25),
-          child: Row(
-            children: <Widget>[
-              Container(
-                  margin: const EdgeInsets.only(right: 20),
-                  child: Icon(Icons.add_circle, size: 40, color: Colors.black)),
-              Expanded(
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                    Text(
-                      'Categoría',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    Text('6 recursos')
-                  ]))
-            ],
-          )));
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        color: MfColors.primary[100],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Encuentra tu ayuda',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            ),
+            Text('Busca recursos sociales gratuitos.')
+          ],
+        ),
+      ),
       Container(
         child: Text('BUSCA RECURSOS EN LA CATEGORÍAS'),
         padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
@@ -38,7 +66,7 @@ class CategoryList extends StatelessWidget {
           child: ListView(
               children: List.generate(10, (index) {
         return Container(
-            padding: const EdgeInsets.all(10), child: categoryCard);
+            padding: const EdgeInsets.all(10), child: _categoryCard(context));
       })))
     ]);
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'layout.dart';
+
 import 'resource.dart';
 
 void main() {
