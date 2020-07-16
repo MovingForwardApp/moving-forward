957fd55d03fa6044d9a724d77541df50964774f0
maria.cano <maria.cano@kaleidos.net>
2020-07-16 14:25:08 +0200

    wip languages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 import 'package:moving_forward/localization.dart';
-
+import 'package:moving_forward/change_language.dart';
 import 'package:moving_forward/layout.dart';
 
 void main() async {
@@ -13,30 +14,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        localizationsDelegates: [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        supportedLocales: [
-          Locale('ar', 'AR'), // Árabe
-          Locale('en', 'US'), // Inglés
-          Locale('es', 'ES'), // Español
-          Locale('fr', 'FR'), // Francés
-        ],
-        title: 'Persons Moving Forward',
-        home: AppLayout(),
-        theme: ThemeData(
-          textTheme: GoogleFonts.madaTextTheme(
-            Theme.of(context).textTheme,
-          ),
-        ),
-        // initialRoute: '/lang',
-        // routes: <String, WidgetBuilder>{
-        //   "/": (BuildContext context) => AppLayout(),
-        //   "/lang": (BuildContext context) => AppLang(),
-        // },
-        debugShowCheckedModeBanner: false);
+    return ChangeNotifierProvider(
+        create: (context) => AppLanguage(),
+        child: MaterialApp(
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+            supportedLocales: [
+              Locale('ar', 'AR'), // Árabe
+              Locale('en', 'US'), // Inglés
+              Locale('es', 'ES'), // Español
+              Locale('fr', 'FR'), // Francés
+            ],
+            title: 'Persons Moving Forward',
+            // home: AppLayout(),
+            theme: ThemeData(
+              textTheme: GoogleFonts.madaTextTheme(
+                Theme.of(context).textTheme,
+              ),
+            ),
+            initialRoute: '/lang',
+            routes: <String, WidgetBuilder>{
+              "/": (BuildContext context) => AppLayout(),
+              "/lang": (BuildContext context) => AppLang(),
+            },
+            debugShowCheckedModeBanner: false));
   }
 }
