4fd228099fa67580ff9e6d173412bb13e77732c4
Xaviju <xaviju@gmail.com>
2020-07-17 10:56:14 +0200

    share

--- lib/resource_detail.dart
+++ lib/resource_detail.dart
@@ -7,6 +7,7 @@ import 'package:moving_forward/models/category.dart';
 import 'package:moving_forward/models/resource.dart';
 import 'package:moving_forward/services/location.dart';
 import 'package:moving_forward/theme.dart';
+import 'package:share/share.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ResourceDetail extends StatelessWidget {
@@ -133,21 +134,24 @@ class ResourceDetail extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          if (resource.phone != '') _actionIcon(
-            Icons.call,
-            AppLocalizations.of(context).translate("phone_call"),
-            resource.phone,
-          ),
-          if (resource.email != '') _actionIcon(
-            Icons.mail_outline,
-            AppLocalizations.of(context).translate("send_email"),
-            resource.email,
-          ),
-          if (resource.web != '') _actionIcon(
-            Icons.public,
-            AppLocalizations.of(context).translate("browse_web"),
-            resource.web,
-          ),
+          if (resource.phone != '')
+            _actionIcon(
+              Icons.call,
+              AppLocalizations.of(context).translate("phone_call"),
+              resource.phone,
+            ),
+          if (resource.email != '')
+            _actionIcon(
+              Icons.mail_outline,
+              AppLocalizations.of(context).translate("send_email"),
+              resource.email,
+            ),
+          if (resource.web != '')
+            _actionIcon(
+              Icons.public,
+              AppLocalizations.of(context).translate("browse_web"),
+              resource.web,
+            ),
           _actionIcon(
             Icons.bookmark_border,
             AppLocalizations.of(context).translate("save"),
@@ -187,9 +191,11 @@ class ResourceDetail extends StatelessWidget {
                     subdomains: ['a', 'b', 'c'],
                     */
                     /* MapBox Tile */
-                    urlTemplate: "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+                    urlTemplate:
+                        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
                     additionalOptions: {
-                      "accessToken": "pk.eyJ1IjoiYmFtZWRhIiwiYSI6ImNrNDl2OGh4cjA4dzMzc3A4c2Q2N25wenUifQ.-9r_WubwqOJqqVl1sZdjtg",
+                      "accessToken":
+                          "pk.eyJ1IjoiYmFtZWRhIiwiYSI6ImNrNDl2OGh4cjA4dzMzc3A4c2Q2N25wenUifQ.-9r_WubwqOJqqVl1sZdjtg",
                       "id": "mapbox.streets",
                     },
                   ),
@@ -258,7 +264,7 @@ class ResourceDetail extends StatelessWidget {
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
@@ -320,9 +326,10 @@ class ResourceDetail extends StatelessWidget {
       elevation: 0,
       actions: <Widget>[
         IconButton(
-          icon: const Icon(Icons.search, size: 30, color: MfColors.dark),
+          icon: const Icon(Icons.share, size: 30, color: MfColors.dark),
           onPressed: () {
-            print('SEARCH...');
+            Share.share(
+                'Check out ${resource.name} ${resource.description}, ${resource.address}, ${resource.googlemapUrl}');
           },
         )
       ],
